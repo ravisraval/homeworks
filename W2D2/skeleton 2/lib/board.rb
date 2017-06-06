5d6da24f1820f8161623d7c93c9e51af73631315
Ravi <raviraval@gmail.com>
2017-06-06 09:51:38 -0700

    Complete mancala

--- W2D2/skeleton 2/lib/board.rb
+++ W2D2/skeleton 2/lib/board.rb
@@ -2,58 +2,63 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
-    @cups = Array.new(14) {[]}
     @name1 = name1
     @name2 = name2
+    @cups = Array.new(14) { Array.new }
     place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
     @cups.each_with_index do |cup, idx|
-      4.times {cup << :stone} unless (idx == 6 || idx == 13)
+      next if idx == 6 || idx == 13
+      4.times do
+        cup << :stone
+      end
     end
   end
 
   def valid_move?(start_pos)
-    raise 'Invalid starting cup' unless start_pos.between?(0, 12) && start_pos != 6
-    raise 'Invalid starting cup' if cups[start_pos].empty?
+    raise "Invalid starting cup" if start_pos < 0 || start_pos > 12
+    raise "Invalid starting cup" if @cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
-      # empties cup
-      stones = @cups[start_pos]
-      @cups[start_pos] = []
+    # empties cup
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
 
-      # distributes stones
-      cup_idx = start_pos
-      until stones.empty?
-        cup_idx += 1
-        cup_idx = 0 if cup_idx > 13
-        # places stones in the correct current player's cups
-        if cup_idx == 6
-          @cups[6] << stones.pop if current_player_name == @name1
-        elsif cup_idx == 13
-          @cups[13] << stones.pop if current_player_name == @name2
-        else
-          @cups[cup_idx] << stones.pop
-        end
+    # distributes stones
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+      # places stones in the correct current player's cups
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
       end
-
-      render
-      next_turn(cup_idx)
     end
+
+    render
+    next_turn(cup_idx)
+  end
 
   def next_turn(ending_cup_idx)
     # helper method to determine what #make_move returns
-    if cups[ending_cup_idx].length == 0
-      return :switch
-    elsif ending_cup_idx == 6 || ending_cup_idx || 13
-      return :promopt
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      # ended on store -- get to choose where to start again
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      # ended on empty cup -- switches players' turns
+      :switch
     else
-      return ending_cup_idx
+      # ended on cup with stones in it -- automatically starts there
+      ending_cup_idx
     end
-
   end
 
   def render
@@ -65,18 +70,18 @@ class Board
   end
 
   def one_side_empty?
-    return true if cups[0..5].all? {|cup| cup.empty?}
-    return true if cups[7..12].all? {|cup| cup.empty?}
-    false
+    @cups.take(6).all? { |cup| cup.empty? } ||
+    @cups[7..12].all? { |cup| cup.empty? }
   end
 
   def winner
-    if cups[6].length > cups[13].length
-      @name1
-    elsif cups[13].length > cups[6].length
-      @name2
-    else
+    player1_count = @cups[6].count
+    player2_count = @cups[13].count
+
+    if player1_count == player2_count
       :draw
+    else
+      player1_count > player2_count ? @name1 : @name2
     end
   end
 end
