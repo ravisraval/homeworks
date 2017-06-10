1f2dd7b0e8ac258a01c51369eacfc58def66ecd4
Ravi <raviraval@gmail.com>
2017-06-09 18:52:42 -0700

    Complete LRU cache

---
+++ W2D3/skeleton 2/spec/dessert_spec.rb
@@ -0,0 +1,69 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef", name: "bobby") }
+  let(:brownie) { Dessert.new("brownie", 100, chef) }
+
+
+  describe "#initialize" do
+    it "sets a type"
+      expect(brownie.type).to_eq("brownie")
+    end
+    it "sets a quantity"
+      expect(brownie.quantity).to_eq(100)
+    end
+    it "starts ingredients as an empty array"
+      expect(brownie.ingredients).to be_empty
+    end
+    it "raises an argument error when given a non-integer quantity"
+      expect { Desser.new("pie", "a lot", chef) }.to raise_error(ArgumentError)
+    end
+
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array"
+      brownie.add_ingredient("chocolate")
+      expect(brownie.ingredients).to include("chocolate")
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+      ingredients = ["chocolate", "flour", "egg", "sugar", "butter"]
+      ingredients.each do |ingredient|
+        brownie.add_ingredient(ingredient)
+      end
+
+      expect(brownie.ingredient).to_eq(ingredients)
+      brownie.mix!
+      expect(brownie.ingredients).not_to eq(ingredients)
+      expect(brownie.ingredients).to eq(ingredients.sort)
+    end
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity"
+      brownie.eat(20)
+      expect(brownie.quantity).to eq(50)
+    end
+    it "raises an error if the amount is greater than the quantity"
+      expect { brownie.eat(120)}.to raise_error("not enough!")
+    end
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name"
+      allow(chef).to receive(:titleize).and_return("Chef Bobby the Great Baker")
+      expect(brownie.serve).to eq("Chef Bobby the Great")
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in"
+  end
+end
